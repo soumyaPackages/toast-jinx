c09b596aab56f15f3b23de5bbb4e310402fdf940
chephyty <soumyahembra123@gmail.com>
2024-12-18 10:45:56 +0530

    make few changes in vite config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,75 @@
 import {} from "react";
-
+import useNotification from "./hooks/use-notification";
+import "./style.css";
 function App() {
-  return <>Hello World</>;
+  const { NotificationComponent, triggerNotification } = useNotification({
+    position: "top-right", // Position of the stack
+  });
+  return (
+    <>
+      <h1>Notification Queue</h1>
+      <button
+        onClick={() =>
+          triggerNotification({
+            type: "success",
+            message: "Success Notification",
+            duration: 3000,
+            animation: "fade",
+            onClose: function (): void {
+              throw new Error("Function not implemented.");
+            },
+          })
+        }
+      >
+        Trigger Success
+      </button>
+      <button
+        onClick={() =>
+          triggerNotification({
+            type: "error",
+            message: "Error Notification",
+            duration: 5000,
+            animation: "slide",
+            onClose: function (): void {
+              throw new Error("Function not implemented.");
+            },
+          })
+        }
+      >
+        Trigger Error
+      </button>
+      <button
+        onClick={() =>
+          triggerNotification({
+            type: "info",
+            message: "Info Notification",
+            duration: 4000,
+            animation: "pop",
+            onClose: function (): void {
+              throw new Error("Function not implemented.");
+            },
+          })
+        }
+      >
+        Trigger Info
+      </button>
+      <button
+        onClick={() =>
+          triggerNotification({
+            type: "warning",
+            message: "Warning Notification",
+            duration: 4000,
+            animation: "pop",
+            onClose: function (): void {
+              throw new Error("Function not implemented.");
+            },
+          })
+        }
+      >
+        Trigger Warning
+      </button>
+      {NotificationComponent}
+    </>
+  );
 }
-
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
           'react-dom': 'ReactDOM'
         },
         assetFileNames: (assetInfo) => {
-          if (assetInfo.name === 'style.css') return 'index.css';
+          if (assetInfo.name === 'style.css') return 'style.css';
           return `assets/[name][extname]`;
         }
       }
